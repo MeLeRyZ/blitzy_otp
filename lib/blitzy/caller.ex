1de8f8d5fafd7b62318b4a0fc58aef9dbaad031a
John Weaver <meleryz@gmail.com>
2018-08-26 17:47:48 +0300

    You can spawn N workers to send GET-requests to specific server.

---
+++ lib/blitzy/caller.ex
@@ -0,0 +1,20 @@
+defmodule Blitzy.Caller do
+    @moduledoc """
+    Creates an amount of workers to sen GET to one url.
+
+    iex> Blitzy.Caller.start(50, "google.com")
+        ...
+    """
+    def start(n_workers, url) do
+        me = self
+
+        1..n_workers
+        |> Enum.map(fn _ -> spawn(fn -> Blitzy.Worker.start(url, me) end) end)
+        |> Enum.map(fn _ ->
+            receive do
+                x -> x
+            end
+        end)
+    end
+
+end
---
+++ lib/blitzy/worker.ex
@@ -0,0 +1,50 @@
+defmodule Blitzy.Worker do
+    @moduledoc """
+    This module is a worker which do the measurement of servers respond.
+
+    When everything goes OK:
+    iex> {:ok, %{status_code: code}
+    If not:
+    {:error, reason}
+    """
+    use Timex
+    require Logger
+
+
+    def start(url, caller, func \\ &HTTPoison.get/1) do
+        {timestamp, response} = Duration.measure(fn -> func.(url) end)
+        caller
+        |> send({self, handle_response({Duration.to_milliseconds(timestamp), response})})
+    end
+
+    ### work with response
+
+    @doc """
+    17:28:01.149 [info]  worker [nonode@nohost-#PID<0.876.0>] completed in 770.104msecs
+    """
+    defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}})
+        when code >= 200 and code <= 304 do
+            Logger.info "worker [#{node}-#{inspect self}] completed in #{msecs}msecs"
+            {:ok, msecs}
+    end
+
+    @doc """
+    17:29:20.046 [info]  worker [nonode@nohost-#PID<0.876.0>] error due to %HTTPoison.Error{id: nil, reason: :nxdomain}
+    {:error, %HTTPoison.Error{id: nil, reason: :nxdomain}}
+    """
+    defp handle_response({_msecs, {:error, reason}}) do
+        Logger.info "worker [#{node}-#{inspect self}] error due to #{inspect reason}"
+        {:error, reason}
+    end
+
+    @doc """
+    {:ok, 770.104}
+    """
+    defp handle_response({_msecs, _}) do
+        Logger.info "worker [#{node}-#{inspect self}] errored out"
+        {:error,:unknown}
+    end
+
+    ### work with response
+
+end
